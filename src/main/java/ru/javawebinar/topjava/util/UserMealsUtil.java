dbe88133098a012dbfe6bc0c50ab182dea276b30
Inlur Gizzatullin <gizzatullinif@gmail.com>
2019-09-29 19:42:43 +0300

    use recursion in Optional2 task

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -13,21 +13,23 @@ import java.util.stream.*;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
         System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
         System.out.println(getFilteredWithExceededStreamed(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
         System.out.println(getFilteredWithExceededOneLoop(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(getFilteredWithExceededWithRecursion(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
         System.out.println(getFilteredWithExceededOneStream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
         System.out.println("****************************************************");
         System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000));
         System.out.println(getFilteredWithExceededStreamed(mealList, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000));
         System.out.println(getFilteredWithExceededOneLoop(mealList, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000));
+        System.out.println(getFilteredWithExceededWithRecursion(mealList, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000));
         System.out.println(getFilteredWithExceededOneStream(mealList, LocalTime.of(7, 0), LocalTime.of(14, 0), 2000));
     }
 
@@ -99,5 +101,30 @@ public class UserMealsUtil {
                                     isExceeded));
                 })
                 .collect(Collectors.toList());
+    }
+
+    private static Map<LocalDate, Integer> dailyCaloriesMap;
+
+    public static List<UserMealWithExceed> getFilteredWithExceededWithRecursion(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        dailyCaloriesMap = new HashMap<>();
+        return filterWithRecursion(mealList, startTime, endTime,caloriesPerDay, 0);
+    }
+
+    private static List<UserMealWithExceed> filterWithRecursion(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay, int index) {
+        List<UserMealWithExceed> result = new ArrayList<>();
+        UserMeal meal = mealList.get(index);
+        dailyCaloriesMap.merge(meal.getLocalDate(), meal.getCalories(), Integer::sum);
+        if (index + 1 < mealList.size()) {
+            result.addAll(filterWithRecursion(mealList, startTime, endTime, caloriesPerDay, index + 1));
+        }
+        if (TimeUtil.isBetween(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+            UserMealWithExceed userMealWithExceed = new UserMealWithExceed(meal.getDateTime(),
+                    meal.getDescription(),
+                    meal.getCalories(),
+                    dailyCaloriesMap.get(meal.getDateTime().toLocalDate()) > caloriesPerDay
+            );
+            result.add(userMealWithExceed);
+        }
+        return result;
     }
 }
